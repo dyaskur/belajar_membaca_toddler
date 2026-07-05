6fc8222bc1f6e1fe2802b4245ce44e071453ce3c
️Yaskur Dyas⚔⚔️⚔ <dyas@yaskur.com>
2026-07-05 20:53:06 +0700

    Revert "test: intentionally break /abjad (will be reverted)"
    This reverts commit c5e0aa56fb6951ae50259ca555bff06e5ac87218.

--- src/routes/abjad/+page.svelte
+++ src/routes/abjad/+page.svelte
@@ -1,5 +1,4 @@
 <script>
-  throw new Error('CI smoke-test canary: intentional break');
   import { goto } from '$app/navigation';
   import { base } from '$app/paths';
   import { browser } from '$app/environment';
